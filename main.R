da1683cb102c1c1005aed81b7cc3f7b068a747e7
Victoria Witthoff <victoriaw@test.noacs.io>
2022-09-16 01:06:17 +0200

    making a test table one

--- main.R
+++ main.R
@@ -1,16 +1,16 @@
 ## Load 
 library("rofi")
 library("dplyr")
-library("labelled")
-library("tableone")
+library(labelled)
+library(table1)
+library(printr, quietly=TRUE)
 source("functions/clean_data.r")
+source("functions/make_table_one.r")
 noacsr::source_all_functions()
 
 
 ## Step 1: Import data
 datasets <- import_data(test = TRUE)
-
-
 
 ## Step 2: Merge data? 
 ## I did this ugly workaround to make the merge_data function in the rofi package work, but perhaps I'm not supposed to use this function
@@ -25,13 +25,11 @@ datasets[['kvalgranskning2014.2017']] <- datasets$kvalgranskning2014.2017_scramb
 
 merged_data <- merge_data(datasets)
 
-
 ## Step 3: Clean data, see functions/clean_data.r
 
-cleaned_data <- clean_data(merged_data)
-
+cd <- clean_data(merged_data)
 
 ## Step 4: Table 1. Patient demographics.
-## Age, sex, ISS, NISS, ASA, penetrating/blunt, what more?? 
 
+t1 <- make_table_one(cd)
 
