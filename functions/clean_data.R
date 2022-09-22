00b320be4b45da6083847d3aec306ae66770c1aa
Victoria Witthoff <victoriaw@test.noacs.io>
2022-09-22 18:01:35 +0200

    working on cleaning data, this is probably all wrong in a variety of ways but at least it doesnt throw any errors

--- functions/clean_data.R
+++ functions/clean_data.R
@@ -1,25 +1,53 @@
 ## All values that are necessary:
-##  For TRISS: ISS, pt_age_years, inj_dominant (blunt or penetrating), RTS: ed_gcs_sum (GCS on arrival at hosp), ed_sbp_value (systolic BP), ed_rr_value (RR)
-##  For NORMIT: NISS, pt_age_years, pt_asa_preinjury, RTS: ed_gcs_sum (GCS on arrival at hosp), ed_sbp_value (systolic BP), ed_rr_value (RR)
-##  For OFI: VK_avslutad, problemomrade_.FMP
-##  Interpretation of OFI stuff:
-##  VK_avslutad -- Whether quality review was done ja/nej
-##  Problemomrade_.FMP -- Whether there was OFI, OK = no OFI
-##  tra_Dodsfallanalysgenomford -- 
+##  For TRISS: ISS, pt_age_yrs, inj_dominant (blunt or penetrating), RTS: ed_gcs_sum (GCS on arrival at hosp), ed_sbp_value (systolic BP), ed_rr_value (RR)
+##  For NORMIT: NISS, pt_age_yrs, pt_asa_preinjury, RTS: ed_gcs_sum (GCS on arrival at hosp), ed_sbp_value (systolic BP), ed_rr_value (RR)
 ##  Add numbers for numbers excluded at each step 
 ##  So need to return a list? HMMMMM
-## For GCS = 99, take prehosp value, (?) exclude if no prehosp value
+##  For GCS = 99, take prehosp value, (?) exclude if no prehosp value
 
-clean_data <- function(dirty.data) {
+clean_data <- function(dirty.data, numbers = FALSE) {
   
-  ## Remove rows with NA in required fields
-  cleaned.data <- dirty.data %>% filter_at(vars(ISS, NISS, pt_age_yrs, inj_dominant, ed_gcs_sum, ed_sbp_value, ed_rr_value, pt_asa_preinjury, Problemomrade_.FMP),all_vars(!is.na(.)))
+  ## Make table smaller overall, kept some ?unnecessary value in case they're possibly interesting to have in tableone
+  ## Also didn't interact with any of the ofi-adjacent variables like VK_avslutad bc it seems as tho create_ofi handles all of that
+  cleaned.data <- dirty.data[,c("pt_Gender", "res_survival", "host_care_level", "inj_dominant", "ofi", "Fr1.12", "pt_asa_preinjury", "ISS", "NISS", "ed_gcs_sum", "ed_rr_value", "ed_sbp_value", "pre_gcs_sum", "pt_age_yrs")]
+  
+  ## Get the total number of rows that we begin with, so I can count how many patients have been removed later
+  included.numbers <- c(original = nrow(cleaned.data))
   
   ## Remove cases where OFI is NA
   cleaned.data <- cleaned.data %>% filter (ofi != "NA")
   
-  ## Remove rows where required values are unknown & filter away age groups. Systolic blood pressure should prolly be >0, unless the patient is DOA? Same for RR.
-  cleaned.data <- cleaned.data %>% filter(inj_dominant != 999 & ed_gcs_sum != 999 & ed_sbp_value > 0 & ed_rr_value > 0 & pt_asa_preinjury != 999 & pt_age_yrs >= 15)
+  ## this is about to get very ugly
+  included.numbers["ofi"] <- nrow(cleaned.data)
   
+  ## Remove where age <15 
+  cleaned.data <- cleaned.data %>% filter (pt_age_yrs >= 15)
+  included.numbers["age"] <- nrow(cleaned.data)
+  
+  ## Remove DOA, not sure what to do if NA/999
+  cleaned.data <- cleaned.data %>% filter (Fr1.12 != 1)
+  included.numbers["doa"] <- nrow(cleaned.data)  
+
+  ## Remove rows with NA in required fields
+  cleaned.data <- cleaned.data %>% filter_at(vars(ISS, NISS, pt_age_yrs, ed_gcs_sum, inj_dominant, ed_sbp_value, ed_rr_value, pt_asa_preinjury),all_vars(!is.na(.)))
+  
+  ## Remove rows where required values are unknown or 0 (doa)
+  cleaned.data <- cleaned.data %>% filter(inj_dominant != 999 & ed_sbp_value > 0 & ed_rr_value > 0 & pt_asa_preinjury != 999 & ed_gcs_sum != 999)
+  
+  ## Need either an ED GCS or a prehosp GCS, already removed all where ed_gcs_sum = 999 or NA
+  v <- c(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
+  cleaned.data <- cleaned.data %>% filter(pre_gcs_sum %in% v | ed_gcs_sum %in% v)
+  included.numbers["parameters"] <- nrow(cleaned.data)
+  
+  ## make numbers of patients excluded at every step in a in a very roundabout fashion
+  excluded.numbers <- c(ofi = included.numbers["original"] - included.numbers["ofi"])
+  excluded.numbers["age"] <- included.numbers["ofi"] - included.numbers["age"]
+  excluded.numbers["doa"] <- included.numbers["age"] - included.numbers["doa"]
+  excluded.numbers["parameters"] <- included.numbers["doa"] - included.numbers["parameters"]
+
+  if (numbers == TRUE) {
+    return (excluded.numbers)
+  } else {
   return (cleaned.data)
+  }
 }
--- main.R
+++ main.R
@@ -5,6 +5,7 @@ library(labelled)
 library(table1)
 library(printr, quietly=TRUE)
 library(DiagrammeR)
+library(purrr)
 
 ## list of libraries i tried to install but failed, mb try again later
 ## rsvg/diagrammersvg, kableextra
@@ -12,11 +13,7 @@ library(DiagrammeR)
 ## reminder: needs to be .R i.e. capital R 
 noacsr::source_all_functions()
 
-## Step 1: Import data
-
 datasets <- import_data(test = TRUE)
-
-## Step 2: Merge data
 
 ## Make dataset names that match merge_data() requirements
 datasets[['swetrau']] <- datasets$swetrau_scrambled
@@ -27,14 +24,16 @@ datasets[['kvalgranskning2014.2017']] <- datasets$kvalgranskning2014.2017_scramb
 
 merged.data <- merge_data(datasets)
 
-## Step 3: Create OFI variable
-
 merged.data$ofi <- create_ofi(merged.data)
 
-## Step 3: Clean data, see functions/clean_data.r
-
 cleaned.data <- clean_data(merged.data)
+exclusion.numbers <- clean_data(merged.data, numbers = TRUE)
 
-## Step 4: Table 1. Patient demographics.
 
-t1 <- make_table_one(cleaned.data)
+## Step 5: Make a little table
+
+
+
+## Step : Table 1. Patient demographics.
+
+## t1 <- make_table_one(cleaned.data)
