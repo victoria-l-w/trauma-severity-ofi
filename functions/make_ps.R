c4c3d441b8421fa517ec89e6f87dd1e1459e1177
Victoria Witthoff <victoriaw@moil.noacs.io>
2022-11-16 17:02:12 +0100

    fix

--- functions/make_ps.R
+++ functions/make_ps.R
@@ -1,27 +1,75 @@
-## Coefficients from Figure 1 here: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5813212/
-## i.e. NORMIT 2
+## https://www.tarn.ac.uk/Content.aspx?c=1895
 
-make_normit <- function(input.data) {
-  
-  lookup <- data.frame(
-    asa = c(1, 2, 3, 4),
-    b1 = c(-0.0713, -0.0565, -0.0487, -0.0081),
-    b2 = c(0.6266, -0.2142, -0.8971, -3.8746)
-  )
+make_ps <- function (input.data) {
   
   ## Cast variables to numeric to ensure they can be used for calculations  
-  n.niss <- as.numeric(input.data["niss"])
-  n.rts <- as.numeric(input.data["rts"])
-  n.asa <- as.numeric(input.data["asa"])
-  n.age <- as.numeric(input.data["age"])
+  ed.gcs <- as.numeric(input.data["ed.gcs"])
+  gcs <- as.numeric(input.data["gcs"] )
+  age <- as.numeric(input.data["age"])
+  gender <- as.numeric(input.data["gender"])
+  iss <- as.numeric(input.data["iss"])
   
-  ## Select the appropriate coefficients based on asa
-  coef <- lookup[lookup$asa == n.asa,]
+  b0 <- 4.9146
   
-  ## Calculate NORMIT!
-  niss.val <- (n.niss * coef$b1) + coef$b2
-  b <- (0.5562 * n.rts) - (4.3234 * ((n.age + 1)/100)^3) + niss.val
-  out <- 1 / (1 + exp(b * -1))
+  ## ed.gcs == 99 -> intubated
+  if (ed.gcs == 99) {gcs.val <- -2.31186} ## b6
+  else { 
+    if (ed.gcs >= 13) {gcs.val <- 0} ## b1
+    else if (ed.gcs >= 9) {gcs.val <- -1.27734} ## b2
+    else if (ed.gcs >= 5) {gcs.val <- -1.68936} ## b3
+    else if (ed.gcs >= 4) {gcs.val <- -2.52661} # b4
+    else {gcs.val <- -3.62339} #b5
+  }
+  
+  ## coefficient for gender
+  if (gender == 1) {gender.val <- 0} # b9
+  else {gender.val <- -0.024416} #b10
+  
+  
+  ## coefficient for age; ignored coefficients for ages <15
+  if (age > 74) {age.val <- -3.01315} ## b18
+  else if (age > 64) {age.val <- -1.74081} ## b17
+  else if (age > 54) {age.val <- -0.995816} ## b16
+  else if (age > 44) {age.val <- -0.557926} ## b15
+  else if (age > 15) {age.val <- 0} ## b14
+  else {age.val <- 0.210467} ## b13
+  
+  if (gender == 2) {
+    if (age > 74) {gen.age.val <- 0.299887} ## b26
+    else if (age > 64) {gen.age.val <- 0.081554} ## b25
+    else if (age > 54) {gen.age.val <- -0.085054} ## b24
+    else if (age > 44) {gen.age.val <- 0.003235} ## b23
+    else if (age > 15) {gen.age.val <- 0} ## b22
+    else {gen.age.val <- -0.23219} ## b21
+  } else {gen.age.val <- 0} ## 0 if male
+  
+  ## Cast variables to numeric to ensure they can be used for calculations
+  gcs.val <- as.numeric(gcs.val)
+  gender.val <- as.numeric(gender.val)
+  age.val <- as.numeric(age.val)
+  gen.age.val <- as.numeric(gen.age.val)
+  
+  ## Calculate the coefficients based on ISS
+  if (iss == 0) {
+    iss1 <- -0.8636 * -3.000163 ## b7
+    iss2 <- -0.2933 * -2.74522 ## b8
+  } else {
+    iss1 <- sqrt(10/iss) - 0.8636
+    iss1 <- iss1 * -3.000163 ## b7
+    iss2 <- log(iss/10) - 0.2933
+    iss2 <- iss2 * -2.74522 ## b8
+  }
+  
+  iss1 <- as.numeric(iss1)
+  iss2 <- as.numeric(iss2)
+  
+  iss.val <- iss1 + iss2
+  iss.val <- as.numeric(iss.val)
+  
+  ## Calculate ps
+  b <- b0 + gcs.val + gender.val + age.val + gen.age.val + iss.val
+  b <- as.numeric(b)
+  out <- exp(b) / (1 + exp(b))
   
   out <- as.numeric(out)
   assertthat::assert_that(length(out) == 1)
